c4337a95ef7aa4c6763a04cdbc1aa58d3627b0d5
tatonico <tato_izraelski@hotmail.com>
2019-08-17 20:04:04 -0300

    Actualizacion

---
+++ enums.c
@@ -0,0 +1,3 @@
+enum Token{ID, BUG, CTE};
+
+enum Estado{INICIAL = 0, IDENTIFICADOR = 1, CONSTANTE = 1, ERROR = 2, FINAL = 3};
--- main.c
+++ main.c
@@ -1,1 +1,34 @@
-bvcbcv
+
+enum Token{ID, BUG, CTE};
+
+Token reconocerToken(void){
+    estado=INICIAL;
+    while !pararSegun(estado){
+        char caracter = leerCaracter();
+        estado = matEstados[estado][caracter];
+    }
+    if elEstadoEsAceptor(estado){
+        if charCentinela(caracter);
+            unput(caracter);
+        Token tok = retornarToken(estado);
+        switch tok{
+            case ID:
+                cantidadIdentificadores++;
+                break;
+            case CTE:
+                cantidadConstantes++;
+                break;
+        }
+    } else {
+        error(estado, caracter);
+        Token tok = BUG;
+        cantidadErrores++;
+    }
+    return tok;
+}
+
+void imprimirDatos(void){
+    printf("%d", "La cantidad de identificadores fue: " +cantidadIdentificadores +
+           "\nLa cantidad de errores fue: " + cantidadErrores + "\nLa cantidad de constantes fue: " + cantidadConstantes);
+}
+
